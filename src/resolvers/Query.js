b4374cc44e84988b744e58cbcf4952cc1b764059
Rizky Ardi Ilhami <rizky.ilhami@kitalulus.com>
2023-03-14 09:57:59 +0700

    add authentication

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,11 @@ function feed(parent, args, context) {
   return context.prisma.link.findMany();
 }
 
+function link(parent, args, context) {
+  return context.prisma.link.findUnique({ where: { id: +args.id } });
+}
+
 module.exports = {
   feed,
+  link,
 };
